b209ccbf3403321b042630b3d5ac8d7beb7478b6
username <younguk137@naver.com>
2023-05-22 09:57:21 +0900

    flow_pop

--- float_pop.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-
-
-
-df = pd.read_csv("/Users/hanyoungwook/2023-1/capstone_design/필요한 데이터들/2022 생활이동 정보/LM_202204/생활이동_행정동_2022.04_18시.csv",encoding='cp1252')
-print (len(df))
-print (df)
